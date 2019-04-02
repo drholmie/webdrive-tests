d1ec926616d4346c06b092edb2d3ae1c238f3c73
drholmie <animesh.leo@gmail.com>
2019-04-02 20:58:48 +0530

    Removed references to auto generated class names
    Made the references to DOM objects independent of generated class names

--- sidebar-buttons.js
+++ sidebar-buttons.js
@@ -1,10 +1,9 @@
 const assert = require('assert');
-const fetch = require('node-fetch');
 describe('linkerd dashboard', () => {
-    it('should have the right title', () => {
-        browser.url('http://127.0.0.1:8000'); //running dashboard on port 8000
-        const sidebar= $(".jss91.jss92.jss4");
-        const menu = sidebar.$$("ul")[0];
+    it('should have the sidemenu', () => {
+        browser.url('http://127.0.0.1:8000');
+        const sidebar= $$("[role=menu]");
+        const menu = sidebar[0];
         assert.equal(menu.$$("a")[0].getText(), 'Overview');
         assert.equal(menu.$$("a")[1].getText(), 'Tap');
         assert.equal(menu.$$("a")[2].getText(), 'Top');
@@ -13,7 +12,8 @@ describe('linkerd dashboard', () => {
         const resources = menu.$$("li")[0];
         assert.equal(resources.getText(), 'Resources');
         resources.click();
-        const sub_menu=$("div.jss126");
+        browser.pause(3000);
+        const sub_menu=resources.$(function(){return this.nextSibling});
         if(sub_menu.isExisting()){
         assert.equal(sub_menu.$$("li")[0].getText(), 'Authorities');
         assert.equal(sub_menu.$$("li")[1].getText(), 'Deployments');
@@ -24,7 +24,8 @@ describe('linkerd dashboard', () => {
         assert.equal(sub_menu.$$("li")[6].getText(), 'Stateful Sets');
         sub_menu.$$("li").forEach((item)=>{
              item.click();
-             item_sub_menu=sub_menu.$("div.jss162.jss163");
+             browser.pause(3000);
+             item_sub_menu=item.$(function(){return this.nextSibling});
              ind=item_sub_menu.$$('div')[0].getText().search("All*");
              assert.notEqual(-1,ind);
              item.click();
