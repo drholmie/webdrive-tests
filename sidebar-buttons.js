f6c3c0722edc28b11bb2a4bf0f5abf0df1955085
drholmie <animesh.leo@gmail.com>
2019-04-14 21:32:54 +0530

    Final sidebar test

--- sidebar-buttons.js
+++ sidebar-buttons.js
@@ -1,36 +1,43 @@
 const assert = require('assert');
 describe('linkerd dashboard', () => {
-    it('should have the sidemenu', () => {
-        browser.url('http://127.0.0.1:8000');
-        const sidebar= $$("[role=menu]");
+    it('should have the right title', () => {
+        browser.url('http://127.0.0.1:7777');
+        sidebar= $$("[role=menu]");
         const menu = sidebar[0];
         assert.equal(menu.$$("a")[0].getText(), 'Overview');
         assert.equal(menu.$$("a")[1].getText(), 'Tap');
         assert.equal(menu.$$("a")[2].getText(), 'Top');
         assert.equal(menu.$$("a")[3].getText(), 'Top Routes');
         assert.equal(menu.$$("a")[4].getText(), 'Service Mesh');
-        const resources = menu.$$("li")[0];
+        const resources = menu.$("li");
         assert.equal(resources.getText(), 'Resources');
         resources.click();
         browser.pause(3000);
-        const sub_menu=resources.$(function(){return this.nextSibling});
-        if(sub_menu.isExisting()){
-        assert.equal(sub_menu.$$("li")[0].getText(), 'Authorities');
-        assert.equal(sub_menu.$$("li")[1].getText(), 'Deployments');
-        assert.equal(sub_menu.$$("li")[2].getText(), 'Daemon Sets');
-        assert.equal(sub_menu.$$("li")[3].getText(), 'Namespaces');
-        assert.equal(sub_menu.$$("li")[4].getText(), 'Pods');
-        assert.equal(sub_menu.$$("li")[5].getText(), 'Replication Controllers');
-        assert.equal(sub_menu.$$("li")[6].getText(), 'Stateful Sets');
-        sub_menu.$$("li").forEach((item)=>{
+        sidebar= $$("[role=menu]");
+        const sub_menu=sidebar[1];
+        list=sub_menu.$$("[role=menuitem]");
+        assert.equal(list[0].getText(), 'Authorities');
+        assert.equal(list[1].getText(), 'Deployments');
+        assert.equal(list[2].getText(), 'Daemon Sets');
+        assert.equal(list[3].getText(), 'Jobs');
+        assert.equal(list[4].getText(), 'Namespaces');
+        assert.equal(list[5].getText(), 'Pods');
+        assert.equal(list[6].getText(), 'Replication Controllers');
+        assert.equal(list[7].getText(), 'Stateful Sets');
+        for(let item of list){
+             item.scrollIntoView();
              item.click();
              browser.pause(3000);
-             item_sub_menu=item.$(function(){return this.nextSibling});
+             item_sub_menu=item.$(function(){return this.nextSibling;});
              ind=item_sub_menu.$$('div')[0].getText().search("All*");
              assert.notEqual(-1,ind);
-             item.click();
-        });
-    }
-       
+        }
+        fin_list=sidebar[2].$$("[role=menuitem]").map(item=>item.getText());
+        assert.equal(fin_list[0],"Documentation");
+        ind=fin_list[1].search("Community");
+        assert.notEqual(-1,ind);
+        assert.equal(fin_list[2],"Join the Mailing List");
+        assert.equal(fin_list[3],"Join us on Slack");
+        assert.equal(fin_list[4],"File an Issue");
 }
-    )});
+)});
